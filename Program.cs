c5e4f45c38b8b1b3bd39839e0194d36ead28a863
mohammad6666m <mohammadmadiamiri@gmail.com>
2024-11-01 20:30:06 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = 40;
+        int n = 20;
+
+        Console.WriteLine($"adad aval bain {n} و {m}:");
+
+        for (int i = n; i <= m; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2) return false; // adad kamtar a 2 aval nis tan
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0) return false; //akar bar i bakhbazier bashad adad aval nist
+        }
+        return true; // dar khai sorat aval and
+    }
+}
+
+
+
+
